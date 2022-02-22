e9b49f73e3daccb8d644b876ba1fdd7f79fe26e2
Paul <paul.cevoli@gmail.com>
2022-02-22 06:27:01 -0500

    C# Practice with C#

---
+++ C#/Project Euler/01 Multiples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Multiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if ( (i % 3 == 0) || (i % 5 == 0))
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("The sum of multiples of 3 and 5 for numbers less than 1000 is {0}", sum);
+            Console.Read();
+        }
+    }
+}
---
+++ C#/Project Euler/02 Even Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Even_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0;
+            long fib1 = 0;
+            long fib2 = 1;
+            while (fib1 < 4000000)
+            {
+                long fib = fib1 + fib2;
+
+                if (fib % 2 == 0)
+                {
+                    sum += fib;
+                }
+
+                Console.WriteLine("{0} {1} {2} {3}", fib1, fib2, fib, sum);
+
+                fib1 = fib2;
+                fib2 = fib;
+            }
+
+            Console.WriteLine("The sum is {0}", sum);
+        }
+
+    }
+
+}
